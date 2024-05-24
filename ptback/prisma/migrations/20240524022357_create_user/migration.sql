54813b22d08e72a7cc66f05c8bd9489b24232515
har1ssoN <harissonmagalhaes@gmail.com>
2024-05-23 23:04:22 -0300

    :sparkles: feat: Model e Migration do User

---
+++ ptback/prisma/migrations/20240524022357_create_user/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "department" TEXT NOT NULL,
+    "course" TEXT NOT NULL,
+    "profile_image" BLOB,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
