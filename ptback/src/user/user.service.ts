af69914fc43b9af91ccffe45701af2e515ec99f4
har1ssoN <harissonmagalhaes@gmail.com>
2024-06-04 17:47:15 -0300

    :sparkles: feat: Hashing das senhas dos users

--- ptback/src/user/user.service.ts
+++ ptback/src/user/user.service.ts
@@ -1,41 +1,118 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { userInfo } from 'os';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private prismaService: PrismaService) {}
 
-  async create(data: CreateUserDto) {
-    const user = this.prisma.user.create({
-      data: data,
+  async create(CreateUserDto: CreateUserDto) {
+    const existingUser = await this.prismaService.user.findUnique({
+      where: { email: CreateUserDto.email },
     });
-    return user;
+    if (existingUser) {
+      throw new ConflictException('User with this email already exists');
+    }
+
+    const hashedPassord = await bcrypt.hash(CreateUserDto.password, 10);
+
+    const user = await this.prismaService.user.create({
+      data: {...CreateUserDto, password: hashedPassord}});
+
   }
 
   async findAll() {
-    return await this.prisma.user.findMany();
+    return await this.prismaService.user.findMany(
+      {select: {id: true,
+        name: true, 
+        email: true, 
+        department: true, 
+        course: true, 
+        profile_image: true,
+        Avaliacoes: true,
+        Comentarios: true,
+        createdAt: true,
+        updatedAt: true},
+      }
+    );
   }
 
   async findOne(id: number) {
-    return this.prisma.user.findUnique({
+    const isValidId = await this.prismaService.user.findUnique({
       where: { id },
+    })
+
+    if (!isValidId) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+
+    return await this.prismaService.user.findUnique({
+      where: { id },
+      select: {id: true,
+        name: true, 
+        email: true, 
+        department: true, 
+        course: true, 
+        profile_image: true,
+        Avaliacoes: true,
+        Comentarios: true,
+        createdAt: true,
+        updatedAt: true},
     });
   }
 
-  async update(id: number, data: UpdateUserDto) {
-    const user = this.prisma.user.update({
-      where: { id },
-      data: data,
-    });
+  async findOneByEmail(email: string) {
+    const user = await this.prismaService.user.findUnique({where: { email },})
+    
+    if (!user) {
+      return null;
+    }
+
     return user;
   }
 
-  async remove(id: number) {
-    return this.prisma.user.delete({
+
+  async update(id: number, UpdateUserDto: UpdateUserDto) {
+    const isValidId = await this.prismaService.user.findUnique({where: { id }, })
+    if (!isValidId) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    const hashedPassord = await bcrypt.hash(UpdateUserDto.password, 10);
+    return await this.prismaService.user.update({
       where: { id },
+      data: {...UpdateUserDto, password: hashedPassord},
+      select: {id: true,
+        name: true, 
+        email: true, 
+        department: true, 
+        course: true, 
+        profile_image: true,
+        Avaliacoes: true,
+        Comentarios: true,
+        createdAt: true,
+        updatedAt: true },
+    })
+  }
+
+  async remove(id: number) {
+    const isValidId = await this.prismaService.user.findUnique({where: { id }, })
+    if (!isValidId) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    return await this.prismaService.user.delete({
+      where: { id },
+      select: {id: true,
+        name: true,
+        email: true,
+        department: true,
+        course: true,
+        profile_image: true,
+        Avaliacoes: true,
+        Comentarios: true,
+        createdAt: true,
+        updatedAt: true},
     });
   }
 }
