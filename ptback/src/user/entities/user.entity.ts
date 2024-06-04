90012f81b3ac4a2bbd6fb5781fa4366ea3a68e50
har1ssoN <harissonmagalhaes@gmail.com>
2024-06-04 13:49:13 -0300

    :recycle: refactor: Melhoria no dto do User

--- ptback/src/user/entities/user.entity.ts
+++ ptback/src/user/entities/user.entity.ts
@@ -1,1 +1,21 @@
-export class User {}
+import { IsOptional, IsString } from "class-validator";
+
+export class UserEntity {
+    @IsString()
+    name: string;
+
+    @IsString()
+    email: string;
+
+    @IsString()
+    password: string;
+
+    @IsString()
+    department: string;
+
+    @IsString()
+    course: string; 
+
+    @IsOptional()
+    profile_image?: Buffer;
+}
